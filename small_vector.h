0819a31bd67196a00f7096be3b28bf6946262910
Daniel Langr <daniel.langr@fit.cvut.cz>
2019-10-08 14:36:48 +0200

    Add a template for small_vector.h

---
+++ small_vector.h
@@ -0,0 +1,17 @@
+#ifndef MPC_SMALL_VECTOR
+#define MPC_SMALL_VECTOR
+
+#include <cstddef>
+
+namespace mpc
+{
+
+template <typename T, size_t N>
+class small_vector
+{
+
+}; // class small vector
+
+} // namespace mpc
+
+#endif // MPC_SMALL_VECTOR
