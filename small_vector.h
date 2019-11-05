c9628f3f6861111585cfa051de3d702b16bb8d56
Ladislav Martínek <martilad@fit.cvut.cz>
2019-11-05 10:51:32 +0100

    add intefrace for methods need in tests

--- small_vector.h
+++ small_vector.h
@@ -15,6 +15,11 @@ public:
     size_t size() const{return this->mSize;}
     size_t capacity() const {return this->mCapacity;}
 
+    T* data(){};
+    void reserve(size_t capacity){}
+    void clear (){}
+    void push_back(T item){}
+    T &operator[] (int){}
 
 private:
     size_t mCapacity{};
