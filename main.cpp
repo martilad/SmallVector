1161fb588926acb4c02e3b4a2f385ea0d3ae0c3f
Ladislav Martínek <martilad@fit.cvut.cz>
2019-12-22 11:39:12 +0100

    add test2

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include "tests/BasicTest.cpp"
 #include "tests/AlignTest.cpp"
 #include "tests/Test1_basic_functionality.cpp"
+#include "tests/Test2_value_type.cpp"
 
 int main() {
     basicTest();
     alignTest();
     test1();
+    test2();
+
 }
 
---
+++ tests/Test2_value_type.cpp
@@ -0,0 +1,44 @@
+#include <cassert>
+#include <cstddef>
+
+class Y
+{
+    static size_t created_; // number of created instances
+    static size_t live_;    // number of "live" instances
+public:
+    Y() = delete;
+    Y(int) { created_++; live_++; }
+    Y(const Y&) : Y(0) { }
+    Y(Y&&) : Y(0) { }
+    ~Y() { live_--;}
+
+    static size_t created() { return created_; }
+    static size_t live() { return live_; }
+};
+
+int test2()
+{
+    mpc::small_vector<Y, 2> v;
+
+    assert(Y::created() == 0);
+
+    v.push_back(1);
+
+    assert(Y::created() >= 1);
+    assert(Y::live() == 1);
+
+    v.clear();
+    assert(Y::live() == 0);
+
+    size_t temp = Y::created();
+    v.emplace_back(2);
+    assert(Y::created() == temp + 1);
+    assert(Y::live() == 1);
+    v.reserve(4);
+    assert(Y::created() == temp + 2);
+    assert(Y::live() == 1);
+    return 0;
+}
+
+size_t Y::created_ = 0;
+size_t Y::live_ = 0;
